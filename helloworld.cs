0b017c706536cff6ee6705932152a29b56bef004
larryjiangjiguo <jiangjiguo@gmail.com>
2025-12-10 14:14:15 -0500

    helloworld.ca added

--- helloworld.cs
+++ helloworld.cs
@@ -1,5 +1,27 @@
+<<<<<<< Updated upstream
 using system.io;
 
 public class helloworld{
   
 }
+=======
+using system.io;
+
+class HelloWorld
+{
+    static void Main()
+    {
+        string path = "hello.txt";
+        string content = "Hello, World!";
+        
+        // Write to the file
+        File.WriteAllText(path, content);
+        
+        // Read from the file
+        string readContent = File.ReadAllText(path);
+        
+        // Print the content to the console
+        System.Console.WriteLine(readContent);
+    }
+}
+>>>>>>> Stashed changes
